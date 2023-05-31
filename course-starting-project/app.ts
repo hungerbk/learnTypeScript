b92bcad1545bbe2c5d6b3479d825e0f8e1d287bb
hungerbk <wjantlr@gmail.com>
2023-05-31 16:08:25 +0900

    23. 리터럴 타입

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,17 +1,26 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
+  // 위에서 지정한 "as-number" | "as-text" 텍스트만 값으로 받을 수 있음
+  // 유니언 타입을 결합하는 데에 리터럴 타입을 이용한다
   let result;
-  // 유니언 타입을 사용할 때는 이러한 런타임 타입 체크가 종종 필요함
-  // 유니언 타입을 사용하면 매개변수를 좀더 유연하게 사용할 수 있음
-  // 로직과 타입에 따라 런타임 타입 체크가 필요할 수도 있고 아닐 수 있음! => 실행되는 코드가 바뀔 수 있기 때문에 그 경우에는 필요함
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if ((typeof input1 === "number" && typeof input2 === "number") || resultConversion === "as-number") {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-const combinedAges = combine(30, 24);
+const combinedAges = combine(30, 24, "as-number");
 console.log(combinedAges);
-const combinedNames = combine("Max", "Anna");
+
+const combinedStringAges = combine("30", "24", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
