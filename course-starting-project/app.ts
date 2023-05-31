aaa60b0e72954e4f5e44b3927ea356d9f1f339b9
hungerbk <wjantlr@gmail.com>
2023-05-31 18:02:59 +0900

    27. 타입의 기능을 하는 함수

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -9,11 +9,12 @@ function printResult(num: number) {
 
 printResult(add(5, 12));
 
-let someValue: undefined;
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+// 두 개의 매개변수(타입: 넘버)를 갖고, 넘버를 반환하는 함수만 지정할 수 있음
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
 
-// 타스에서 언디파인드는 타입임
-// 하지만 반환 타입으로 undefined를 사용할 수 없음 (사용하지 않음?)
-// 언디파인드를 사용하는 것은 아무런 값도 반환하지 않는 반환문이 있다는 것을 의미함
-// 언디파인드는 드물게 사용하는데, 실제 값을 반환하지 않을 때 사용할 수 있다
-// 함수가 언디파인드를 비롯하여 반환하는 것이 없다면 void를 사용해야 함 ⇒ 이 함수에 의도적으로 반환문이 없다는 것을 의미하는 것이기 때문
-// 타스가 언디파인드를 추론할 수는 없다. 보이드를 사용하는 것이 표준.
+// let someValue: undefined;
