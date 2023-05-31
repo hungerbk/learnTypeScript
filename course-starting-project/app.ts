f2d6435b560806947dffb35281e7d852b8b72856
hungerbk <wjantlr@gmail.com>
2023-05-31 16:35:45 +0900

    24. 타입 알리어스/사용자 객체 타입

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,4 +1,8 @@
-function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+// 자스와 타스에서 정의되지 않은 이름만 사용할 수 있다 (Date는 자스 내장함수이기 때문에 사용 불가)
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
   // 위에서 지정한 "as-number" | "as-text" 텍스트만 값으로 받을 수 있음
   // 유니언 타입을 결합하는 데에 리터럴 타입을 이용한다
   let result;
