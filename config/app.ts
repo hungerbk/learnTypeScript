060014964d12b339cb5ce53ed5b22372a3f0cb8d
hungerbk <wjantlr@gmail.com>
2023-06-08 15:53:04 +0900

    34. watch 모드

--- config/app.ts
+++ config/app.ts
@@ -1,1 +1,9 @@
-console.log("Time to get started...");
+const userName = "Maximilian";
+userName = 3; //에러
+
+console.log(userName);
+
+// --w 를 통해 관찰모드
+// 파일에 변경사항이 있을 때마다 컴파일 하게 됨
+// 에러 출력
+// 단 파일을 구체적으로 지정해야 하기 때문에 큰 프로젝트에는 적절하지 않음
