c72b144b1e932198fcc74f93830534e72924b6bf
hungerbk <wjantlr@gmail.com>
2023-07-26 17:14:07 +0900

    63. 약식 초기화 (이중 초기화를 간단하게 작성)

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,15 +1,19 @@
 class Department {
-  public name: string;
+  // private id: string;
+  // public name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  // 이중 초기화를 한번에하는 것 원래는 주석처리한 부분(2, 3번 줄 + 9, 10번 줄) 모두 작성해야 하는데, 컨스트럭터 괄호 안의 한 줄로 끝낼 수 있음
+  // 값을 찾은 다음에 인수를 할당해야 하는데, 저렇게 하면 같은 이름의 속성이 생성되고 인수에 대한 값이 속성이 저장되는 것
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   // 아래처럼 메소드에 매개변수를 지정하고, 타입을 지정하면 undefined가 나오는(원치 않는 결과가 나오는) 것을 방지 할 수 있음
   // this.name이 없는 경우 에러가 발생함
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -22,14 +26,14 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 // console.log(accounting);
 accounting.describe();
 
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
 
-accounting.employees[2] = "Anna";
+// accounting.employees[2] = "Anna";
 
 accounting.printEmployeeInformation();
 
