710a7a8602ec1590df55a2fd37eb615ae8cba67e
hungerbk <wjantlr@gmail.com>
2023-08-01 16:39:08 +0900

    95. 제네릭 함수 생성하기

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,20 +1,20 @@
-const names: Array<string> = []; // string[] 와 일치함
+// const names: Array<string> = []; // string[] 와 일치함
 // 제네릭 타입은 다른 타입과 연결된다. 어떤 것과 연결된 것인지는 타스를 통해 알 수 있음
 
-names[0].split(" ");
+// names[0].split(" ");
 // 배열 안에 오는 것이 문자열인 것을 알고 있으면 문자열의 함수를 알 수 있음
 // 배열에 특정 타입을 지정하면 해당 타입의 함수를 쓸 수 있음
 
 // 프로미스 타입
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
 
 // 프로미스 타입은 타스에 있는 것은 아니고 자스에 있는 것임
 // 프로미스 상수가 프로미스 타입임
@@ -25,3 +25,30 @@ promise.then((data) => {
 // 이는 작업을 수행할 때마다 타스의 도움을 받는 데에 사용된다
 // 정보를 미리 아는 데에 사용 할 수 있음!
 // 물론 클래스나 함수에 이용하면 다른 기능으로 쓸 수 있다..!
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+
+// console.log(merge({ name: "Max" }, { age: 30 })); // 이것은 문제 없이 실행됨.
+
+// 문제는 저장할 때임!
+
+// const mergedObj = merge({ name: "Max" }, { age: 30 });
+// mergedObj.name // 접근할 수 없다.
+
+// object가 구체적인 객체가 아니라 어떤 객체든 될 수 있기 때문에 타입스트립트는 인터섹션이 반환된다고 추론할 수 있음
+// 제네릭 타입을 사용하게 되면 두 매개변수가 서로 다른 타입이 될 수도 있다고 알려줄 수 있음
+// 무작위가 아닌 다양한 타입 데이터를 얻는 것이라고 알려주는 것
+// 정확한 객체나 타입을 알 필요가 없음.
+// 특정 객체가 아닌 인터섹션을 반환한다는 것만 알려주면 됨
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB); // 여기 왜 에러가 발생할까..
+}
+// 위 처럼 작성하면 타스가 알아서 타입을 추론함
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj.name);
+// 물론 아래처럼 세부적으로 알려줄 수 있음
+// 그냥 <{}, {}> 이런식으로 하면 에러 발생
+// 하지만 굳이 아래처럼 할 필요는 없다!
+const mergedObj2 = merge<{ name: string; hobbies: string[] }, { age: number }>({ name: "Max", hobbies: ["Cooking}"] }, { age: 30 });
