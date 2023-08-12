c5436345456209f5a4e7ac841cfdd4bcfed5c885
hungerbk <wjantlr@gmail.com>
2023-08-12 21:04:28 +0900

    142. 네임스페이스 작업하기
    - tsconfig.json 에서 outFile 설정 및 module을 amd로 변경
    - index.html 에서 연결된 js파일을 app.js에서 bundle.js로 변경
    
    - 하나의 파일로 컴파일되어야 한다.

---
+++ 10.moduleAndNamespace/src/project-model.ts
@@ -0,0 +1,12 @@
+namespace App {
+  // 우리가 app.ts에서도 사용할 네임스페이스와 이름이 같아야 함
+  // 네임스페이스는 이름이 같은 다른 파일에 있는 네임스페이스에서도 같은 기능을 사용할 수 있음!!
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  export class Project {
+    constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {}
+  }
+}
