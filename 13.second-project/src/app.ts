ca6242bdf317af1db7f635c5ecdd0825c84cd4ea
hungerbk <wjantlr@gmail.com>
2023-08-20 23:46:25 +0900

    170. 사용자 입력 받기

--- 13.second-project/src/app.ts
+++ 13.second-project/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault(); // 기존 submit은 안 되게!
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API!
+}
+
+form.addEventListener("submit", searchAddressHandler);
