7a5f5a51ea040f79cc7cae90b79b3e3bd1737078
hungerbk <wjantlr@gmail.com>
2023-08-21 20:34:29 +0900

    171. 구글 API 설정하기

--- 13.second-project/src/app.ts
+++ 13.second-project/src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+const GLOBAL_API_KEY = process.env.GOOGLE_MAP_API_KEY;
+
 function searchAddressHandler(event: Event) {
   event.preventDefault(); // 기존 submit은 안 되게!
   const enteredAddress = addressInput.value;
