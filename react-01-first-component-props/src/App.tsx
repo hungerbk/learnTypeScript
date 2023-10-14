c303ddd16e9e5dddb34aa694fce81af08b94a588
hungerbk <wjantlr@gmail.com>
2023-10-14 22:59:47 +0900

    181. Cross-Component 커뮤니케이션

--- react-01-first-component-props/src/App.tsx
+++ react-01-first-component-props/src/App.tsx
@@ -7,9 +7,17 @@ import NewTodo from "./components/NewTodo";
 // FC: functional component
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    // 입력하면 App.tsx에서 실행된다.
+    // 여기에 console.log가 있기 때문.
+    // NewTodo와 App 컴포넌트 간 상호작용이 이루어지고 있음
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- react-01-first-component-props/src/components/NewTodo.tsx
+++ react-01-first-component-props/src/components/NewTodo.tsx
@@ -1,6 +1,11 @@
 import React, { useRef } from "react";
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+// onAddTodo 속성을 타입이나 인터페이스로 정의하고 함수에 제네릭 타입 추가
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   // useRef를 실행하면 ref 객체가 반환되고 이를 상수에 할당할 수 있음
   // DOM과 상호작용하게 할 수 있음 (preventDefault()를 했더라도)
   // 렌더링되기 전에는 값이 없음 > 기본값 null을 입력해주는 이유
@@ -11,7 +16,7 @@ const NewTodo: React.FC = () => {
     // 타입스크립트에서는 ref가 연결이 되었는지 확싱할 수 없기 때문에 에러 발생 > !나 ? 넣어줌
     // 렌더링 이후에 서브밋되었을 때만 이 함수가 실행되는 것이기 때문에 null이 아니라는 것을 확신할 수 있음
     const enteredText = textInputRef.current!.value;
-    console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
 
   return (
