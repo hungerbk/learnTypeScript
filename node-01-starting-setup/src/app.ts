46ba409c667335659acb208d5e48d8563f4aee92
hungerbk <wjantlr@gmail.com>
2023-10-18 23:34:47 +0900

    193. 미들웨어 및 타입 추가하기

--- node-01-starting-setup/src/app.ts
+++ node-01-starting-setup/src/app.ts
@@ -1,5 +1,17 @@
-import Express from "express";
+import Express, { Request, Response, NextFunction } from "express";
+
+import todoRoutes from "./routes/todos";
 
 const app = Express();
+
+app.use("/todos", todoRoutes);
+
+// express에서 사용하는 미들웨어 함수
+// app.use((req, res, next) => {});
+
+// NextFunction은 요청이 다음 미들웨어로 차례로 이동하게 하는 함수
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
